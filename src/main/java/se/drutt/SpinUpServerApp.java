78e3c9e60f4b387a7634ac59df53ec9c1f09e46e
andreas <andreas@chalmers.se>
2021-10-27 13:57:58 +0200

    Init of AWS CDK.

---
+++ src/main/java/se/drutt/SpinUpServerApp.java
@@ -0,0 +1,29 @@
+package se.drutt;
+
+import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.StackProps;
+
+public class SpinUpServerApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        new SpinUpServerStack(app, "SpinUpServerStack", StackProps.builder()
+                // If you don't specify 'env', this stack will be environment-agnostic.
+                // Account/Region-dependent features and context lookups will not work,
+                // but a single synthesized template can be deployed anywhere.
+
+                // Uncomment the next block to specialize this stack for the AWS Account
+                // and Region that are implied by the current CLI configuration.
+                /*
+                .env(Environment.builder()
+                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                        .region(System.getenv("CDK_DEFAULT_REGION"))
+                        .build())
+                */
+
+                  // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+                .build());
+
+        app.synth();
+    }
+}
